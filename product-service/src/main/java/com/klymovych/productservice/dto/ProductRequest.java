68661e0018aa021a2abd81c68daf256a527d7256
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-09-05 16:29:22 +0300

    add ProductResponse and ProductRequest, dto for Rest

---
+++ product-service/src/main/java/com/klymovych/productservice/dto/ProductRequest.java
@@ -0,0 +1,19 @@
+package com.klymovych.productservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequest {
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
---
+++ product-service/src/main/java/com/klymovych/productservice/dto/ProductResponse.java
@@ -0,0 +1,21 @@
+package com.klymovych.productservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+}
