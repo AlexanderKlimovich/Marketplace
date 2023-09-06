e18982e2c64d7037209dfc52af3b2a167487858a
AlexanderKlimovich <alexanderklimovich1@gmail.com>
2023-09-06 14:29:07 +0300

    add netflix eureka client to all microservices and update code in Order service for new communication tool

--- order-service/src/main/java/com/klymovych/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/klymovych/orderservice/service/OrderService.java
@@ -25,7 +25,7 @@ public class OrderService {
 
     private final OrderRepository repository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest request) {
         Order order = new Order();
@@ -41,8 +41,8 @@ public class OrderService {
                 .toList();
 
         //call inventory service to check that product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
